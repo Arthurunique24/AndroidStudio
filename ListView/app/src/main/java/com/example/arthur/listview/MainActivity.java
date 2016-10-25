de3fafa39daf2df5639e664610272daffc9bdaab
Arthurunique24 <arthurunique24@gmail.com>
2016-10-25 23:15:49 +0300

    Siple Adapter

---
+++ ListView/app/src/main/java/com/example/arthur/listview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.arthur.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        //простейший адаптер
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
+    }
+}
