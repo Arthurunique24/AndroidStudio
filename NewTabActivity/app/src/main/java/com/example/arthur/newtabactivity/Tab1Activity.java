428bdf9e82234fb9b08f425d23449533a9e39998
Arthurunique24 <arthurunique24@gmail.com>
2016-11-27 11:47:20 +0300

    Creating swipe Tabs with Tabbed Activity

---
+++ NewTabActivity/app/src/main/java/com/example/arthur/newtabactivity/Tab1Activity.java
@@ -0,0 +1,19 @@
+package com.example.arthur.newtabactivity;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Activity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_tab1, container, false);
+        return rootView;
+    }
+}
